70f75e2ecbd7ad1a2ce1cb2a2b9f0504397a99cf
KayJss <152619478+KayJss@users.noreply.github.com>
2024-10-04 17:54:29 +0300

    Update classes.py

---
+++ classes.py
@@ -0,0 +1,4 @@
+class web:
+    link = "https://www.cimer.gov.tr/Hesap/EDevletGiris" #Elleme
+    tc = "Tc Kimlik Numarası" #Tc Kimlik Numaranı gir
+    sifre = "e-devlet Şifresi" #E devlet Şifreni Gir
